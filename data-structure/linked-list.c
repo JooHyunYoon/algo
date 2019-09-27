22f918eb6ebfce5a4cf61c3106bf9eab7f595990
JooHyunYoon <juse22@naver.com>
2019-09-27 18:47:53 +0900

    linked-list

--- data-structure/linked-list.c
+++ data-structure/linked-list.c
@@ -1,10 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node {
     char Data;
     struct node *next;
 } NODE;
 
+NODE *head, *tail;
+
+void initialize(){
+    head = (NODE *)malloc(sizeof(NODE));
+    tail = (NODE *)malloc(sizeof(NODE));
+
+    head->Data = 0;
+    tail->Data = 0;
+
+    head->next = tail;
+    tail->next = NULL;
+}
+
+void insertNode(char n){
+    NODE *index = head;
+    NODE *newNode = (NODE *)malloc(sizeof(NODE));
+    newNode->Data = n;
+    newNode->next = NULL;
+
+    while(index->next != tail){
+        index = index->next;
+    }
+
+    index->next = newNode;
+    newNode->next = tail;
+    head->Data = head->Data + 1;
+}
+
+void deleteNode(char n){
+    NODE *index = head;
+    NODE *delNode = (NODE *)malloc(sizeof(NODE));
+
+    while(index->next->Data != n){
+        index = index->next;
+    }
+
+    delNode = index->next;
+    index->next = delNode->next;
+    
+    head->Data = head->Data - 1;
+    free(delNode);
+}
+
+void printNode(){
+    int i;
+    NODE *index = head;
+
+    for(i = 0; i <= head->Data + 1; i++){
+        if(index != head && index != tail){
+            printf("%d ", index->Data);
+        }
+        index = index->next;
+    }
+
+    printf("\n");
+}
+
 int main(){
-    printf("hello\n");
+    int i;
+    initialize();
+
+    // insert data
+    for(i = 1; i <= 5; i++){
+        insertNode(i);
+    }
+
+    printNode();
+
+    // delete 3
+    deleteNode(3);
+    printNode();
+
+    return 0;
 }
