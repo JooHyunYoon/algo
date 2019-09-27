664fe1a6e7608d77bb304c18977945db86014d45
JooHyunYoon <juse22@naver.com>
2019-09-27 20:49:36 +0900

    quicksort

---
+++ algorithm/quicksort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE    7
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quickSort(int *arr, int start, int end){
+    if(start >= end){
+        return;
+    }
+
+    int left = start + 1;
+    int right = end;
+    int pivot = arr[start];
+
+    while(1){
+        while(arr[left] <= pivot){
+            left++;
+        }
+        
+        while(arr[right] > pivot){
+            right--;
+        }
+
+        if(left > right){
+            break;
+        }
+
+        swap(&arr[left], &arr[right]);
+    }
+    swap(&arr[start], &arr[right]);
+
+    quickSort(arr, start, right - 1);
+    quickSort(arr, right + 1, end);
+}
+
+void printArr(int *arr){
+    int i;
+    for(i = 0; i < SIZE; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int Buf[SIZE] = {3, 45, 5, 8, 23, 67, 13};
+
+    printArr(Buf);
+    quickSort(Buf, 0, SIZE - 1);
+    printArr(Buf);
+    
+    return 0;
+}
